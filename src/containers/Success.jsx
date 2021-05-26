172f876fc3001e1d5589026cc4072715620b79c8
jorge <georgellanque@gmail.com>
2021-05-26 17:16:03 -0400

    Add Payment's component

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 export default function Success() {
-  return <div>SUCCESS</div>;
+  return (
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Jorge, Gracias por tu compra</h2>
+        <span>Tu pedido llegará en 3 dias a tu dirección</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
+    </div>
+  )
 }
