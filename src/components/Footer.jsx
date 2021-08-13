803d4eccf72cad9c6648752951ff1b8654880ca9
jorge <georgellanque@gmail.com>
2021-08-13 09:03:39 -0400

    Add new styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom'
+import { BiHomeAlt, BiCartAlt, BiGroup } from 'react-icons/bi'
+import AppContext from "../context/AppContext"
 import '../styles/components/Footer.css';
 
+
 export default function Footer() {
+  const {state} = useContext(AppContext)
+  const {cart} = state
+
   return (
     <div className="Footer">
-      <p className="Footer-title">Platzi Conf Merch</p>
-      <p className="Footer-copy">Todos los izquierdos reservados</p>
+      <BiHomeAlt className="Footer__Icon"/>
+      <Link to="/checkout" className="Checkout-Wrap">
+        <BiCartAlt className="Footer__Icon"/>
+        {cart.length > 0 && <div className="Checkout-Alert">{cart.length}</div>}
+      </Link>
+      <BiGroup className="Footer__Icon"/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
-import '../styles/components/Header.css';
 import { Link } from 'react-router-dom';
+import { BiSearchAlt2} from 'react-icons/bi';
 import AppContext from '../context/AppContext';
+import logo from "../assets/logo.png"
+import '../styles/components/Header.css';
+
 
 export default function Header() {
   const { state } = useContext(AppContext);
@@ -9,15 +12,18 @@ export default function Header() {
 
   return (
     <div className="Header">
-      <h1 className="Header-title">
-        <Link to="/">Merch</Link>
-      </h1>
-      <div className="Header-checkout">
-        <Link to="/checkout">
-          <i className="fas fa-shopping-basket fa-2x" />
-        </Link>
-        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
+      <Link to="/">
+        <img src={logo} alt="logo-store" className="Header-Logo"/>
+      </Link>
+      <div className="Header-Search">
+        < BiSearchAlt2 className="Icon-Search"/>
       </div>
     </div>
   );
 }
+
+      // <div className="Header-checkout
+        // <Link to="/checkout">
+        //   <i className="fas fa-shopping-basket fa-2x" />
+        // </Link>
+        // {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
