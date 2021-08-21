0336d9b0477c048fb6c040b4b96b4b0d93b2e4f3
jorge <georgellanque@gmail.com>
2021-08-21 18:26:18 -0400

    Add badge offer

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,15 +6,19 @@ export default function Product({ product, handleAddToCart }) {
   return (
     <div className="Products-item">
        <header className="Item__Header">
-          <span className="Item__Offer">30% OFF</span>
+          <div className="Item__Offer">30% OFF</div>
           <AiFillHeart className="Item__Offer-Icon" onClick={handleAddToCart(product)}/>
        </header>
        <div className="Item__Image">
         <img src={product.image} alt={product.title} />
        </div>
       <div className="Product-item-info">
-        <h2>{product.title}</h2>
-        <span className="Item__Price">${(product.price).toFixed(2)}</span>
+        <div className="Product-item-info-rating">rating</div>
+        <h2>{product.title}</h2>  
+        <div className="Product-item-info-detail">
+          <button type="button">Call tu action</button>
+          <span className="Item__Price">${(product.price).toFixed(2)}</span>
+        </div>
       </div>
     </div>
   );
